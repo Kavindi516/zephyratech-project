0a22801aac6979e0fcdf549a47240066749c4163
Kavindi <kavindinanayakkara8303@gmail.com>
2025-06-02 09:29:45 +0530

    Final update for release branch

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,23 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosInstance = axios.create();
-
 
 // Axios instance with request and response interceptors
 axiosInstance.interceptors.request.use(
   (config) => {
-    console.log("Request Intercepted:", config);
+    console.log('Request Intercepted:', config);
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 axiosInstance.interceptors.response.use(
   (response) => {
-    console.log("Response Intercepted:", response);
+    console.log('Response Intercepted:', response);
     return response;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 export default axiosInstance;
