754b833c87227b007c2cbbf020c4615d99926616
Kavindi <kavindinanayakkara8303@gmail.com>
2025-06-02 08:34:29 +0530

    Add updated axiosInstance.jsx

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create();
 
+
+// Axios instance with request and response interceptors
 axiosInstance.interceptors.request.use(
   (config) => {
     console.log("Request Intercepted:", config);
